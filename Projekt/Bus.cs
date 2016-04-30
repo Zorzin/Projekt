8fa391e13626233be47bfb2e1695c7588a2caa85
Szymon <zorzin0os@gmail.com>
2016-04-30 18:04:34 +0200

    Dodane klasy

---
+++ Projekt/Bus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Bus
+    {
+        private int     _busid;
+        private string  _type;
+        private int     _actualdriver;
+        private int     _actualline;
+        private int     _mileage;
+        private string  _brand;
+        private string  _model;
+        private string  _techcondition;
+
+        public Bus(int busid, string type, int actualdriver, int actualline, int mileage, string brand, string model, string techcondition)
+        {
+            _busid = busid;
+            _type = type;
+            _actualdriver = actualdriver;
+            _actualline = actualline;
+            _mileage = mileage;
+            _brand = brand;
+            _model = model;
+            _techcondition = techcondition;
+        }
+
+        public Bus()
+        {
+            _busid = -1;
+            _type = null;
+            _actualdriver = -1;
+            _actualline = -1;
+            _mileage = 0;
+            _brand = null;
+            _model = null;
+            _techcondition = null;
+        }
+}
---
+++ Projekt/Driver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Driver
+    {
+        private string  _name;
+        private string  _secondname;
+        private int     _id;
+        private string  _status;
+        private int     _driverlicenseid;
+        private string  _city;
+        private int     _zipcode;
+        private string  _address;
+        private int     _actualline;
+
+        public Driver(string name, string secondname, int id, string status, int driverlicenseid, string city, int zipcode, string address, int actualline)
+        {
+            _name = name;
+            _secondname = secondname;
+            _id = id;
+            _status = status;
+            _driverlicenseid = driverlicenseid;
+            _city = city;
+            _zipcode = zipcode;
+            _address = address;
+            _actualline = actualline;
+        }
+
+        public Driver()
+        {
+            _name = null;
+            _secondname = null;
+            _id = -1;
+            _status = null;
+            _driverlicenseid = -1;
+            _city = null;
+            _zipcode = -1;
+            _address = null;
+            _actualline = -1;
+        }
+
+
+    }
+}
